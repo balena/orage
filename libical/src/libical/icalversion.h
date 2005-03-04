223b3df51e9a1004aadb48518f194dc64a7c5258
Benedikt Meurer <benny@xfce.org>
2005-03-04 15:25:24 +0000

    Integrate libical with the xfcalendar build process. The included copy of libical is only used if libical isn't detected on the target system. The additional files installed by xfcalendar then (the zoneinfo files) are installed in $(datadir)/xfcalendar/zoneinfo/ to avoid conflicts with a probably installed version of libical.
    (Old svn revision: 2010)

--- libical/src/libical/icalversion.h
+++ libical/src/libical/icalversion.h
@@ -1,7 +1,7 @@
 #ifndef ICAL_VERSION_H
 #define ICAL_VERSION_H
 
-#define ICAL_PACKAGE "libical"
-#define ICAL_VERSION "0.24"
+#define ICAL_PACKAGE "xfcalendar"
+#define ICAL_VERSION "4.3.1.0cvs"
 
 #endif
